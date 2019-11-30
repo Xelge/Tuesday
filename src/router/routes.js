0520db1e6844031db317d732dcf2f68a4e2679e4
pstld <50254165+pstld@users.noreply.github.com>
2019-11-30 23:19:32 +0300

    Add files via upload

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,8 +4,35 @@ const routes = [
     path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      {
+        path: '',
+        component: () => import('pages/home.vue')
+      },
+      {
+        path: 'information',
+        component: () => import('pages/information.vue')
+      },
+      {
+        path: 'help',
+        component: () => import('pages/help.vue')
+      },
+      {
+        path: 'sponsorship',
+        component: () => import('pages/sponsorship.vue')
+      },
+      {
+        path: 'brands',
+        component: () => import('pages/brands.vue')
+      }
     ]
+  },
+  {
+    path: '/registration',
+    component: () => import('pages/registration.vue')
+  },
+  {
+    path: '/login',
+    component: () => import('pages/login.vue')
   }
 ]
 
