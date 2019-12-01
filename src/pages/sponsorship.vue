ad293832fc61de759027b998766295be9ae437e8
pstld <50254165+pstld@users.noreply.github.com>
2019-12-01 14:55:30 +0300

    Add files via upload

--- src/pages/sponsorship.vue
+++ src/pages/sponsorship.vue
@@ -14,35 +14,31 @@
 
   <div class="row justify-center">
     <div class="col-10">
-      <q-card dark class="bg-dark my-card col-10 q-my-xl">
+      <q-card dark flat class="bg-dark my-card col-10 q-my-xl" v-for="(brand,index) in brands" :key="index">
         <q-card-section>
-          <div class="text-h6 bg-white text-black q-py-xs q-px-md card">OMERTA</div>
+          <div class="text-h6 bg-white text-black q-py-xs q-px-md card">{{brand.name}}</div>
           <q-img
             class="card q-my-lg"
-            src="https://placeimg.com/500/300/nature"
+            :src="brand.headimage"
           />
+          <div class="row">
+            <q-btn outline rounded label="Подробнее" class="col-2" to="/brand_about" />
+            <div class="votes col-2 offset-6 q-py-sm">37 голосов</div>
+            <q-btn rounded color="white" text-color="black" label="Проголосовать" class="col-2"  />
+          </div>
         </q-card-section>
       </q-card>
+    </div>
+  </div>
 
-      <q-card dark class="bg-dark my-card col-10 q-my-xl no-border">
-        <q-card-section>
-          <div class="text-h6 bg-white text-black q-py-xs q-px-md card">OMERTA</div>
-          <q-img
-            class="card q-my-lg"
-            src="https://placeimg.com/500/300/nature"
-          />
-        </q-card-section>
-      </q-card>
-
-      <q-card dark class="bg-dark my-card col-10 q-my-xl">
-        <q-card-section>
-          <div class="text-h6 bg-white text-black q-py-xs q-px-md card">OMERTA</div>
-          <q-img
-            class="card q-my-lg"
-            src="https://placeimg.com/500/300/nature"
-          />
-        </q-card-section>
-      </q-card>
+  <div class="text-white text-center justify-center row">
+    <div class="col-6 start">
+      <div class="text-h3 q-pb-lg start-head">СОЗДАЙ СВОЙ БРЕНД</div>
+      <div class="start-description">Тоже хочешь создать свой бренд? Мы поможем найти<br>
+        спонсоров для реализации твоей идеи! Отправляй заявку и, возможно, именно твой бренд увидит свет с помощью нашей команды.</div>
+      <router-link to="/" class="justify-center row">
+        <q-btn class="btn q-my-lg q-mx-lg" outline color="white" label="ОФОРМИТЬ ЗАЯВКУ" />
+      </router-link>
     </div>
   </div>
 
@@ -53,9 +49,15 @@
 </div>
 </template>
 
-<script>
+<script src="../router/massiv.js">
 export default {
-  name: 'sponsorship'
+  name: 'sponsorship',
+  data () {
+    return {
+      model: null,
+      brand: null
+    }
+  }
 }
 </script>
 
@@ -65,7 +67,7 @@ export default {
   background: #161616;
 }
   .start {
-    padding: 207px 0px 207px;
+    padding: 100px 0px 100px;
     &-head {
       font-family: Prosto One;
     }
@@ -88,5 +90,31 @@ export default {
     font-family: Prosto One;
     max-height: 334px;
   }
+  q-btn {
+    font-size: 16px;
+    color: white;
+  }
+  .votes {
+    font-family: Rubik, serif;
+    color: #686868;
+    font-size: 24px;
+  }
+  .btn {
+    font-family: Prosto One;
+    font-size: 24px;
+    display: flex;
 
+    border-radius: 10px;
+  }
+  a {
+    text-decoration: none;
+  }
+  .close {
+    font-size: 24px;
+    position: absolute;
+    right: 25px;
+    top: 25px;
+    cursor: pointer;
+    color: #fff;
+  }
 </style>
