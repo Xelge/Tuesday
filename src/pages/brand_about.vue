6ca3ff58ec4abd259a088a2d4f334058b9df3e40
pstld <50254165+pstld@users.noreply.github.com>
2019-12-01 14:56:01 +0300

    Add files via upload

---
+++ src/pages/brand_about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="brand-content row">
+      <div class="col-10" v-for="(brand,index) in brands" :key="index">
+        <h1>{{brand.name}}</h1>
+        <div>{{brand.description}}</div>
+        <q-btn class="btn q-my-lg q-mx-lg" outline color="black" label="ПРОГОЛОСОВАТЬ" />
+        <q-btn flat round color="black" icon="close" />
+      </div>
+  </div>
+</template>
+
+<script src="../router/massiv.js">
+export default {
+  name: 'brand_about',
+  data () {
+    return {
+      brand: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
